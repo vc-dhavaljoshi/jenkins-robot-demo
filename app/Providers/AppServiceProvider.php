e92b2fa8d89e06aac78209a7cd72eb2b3ee4b763
vc-dhavaljoshi <dhaval.joshi@viitor.cloud>
2019-04-08 14:07:09 +0530

    Robot framework testing test case

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,9 +18,7 @@ class AppServiceProvider extends ServiceProvider
     }
 
     /**
-     * Bootstrap any application services.
      *
-     * @return void
      */
     public function boot()
     {

52f93941d95fbc1eb8b4640f127783332ed6f4dd
vc-dhavaljoshi <dhaval.joshi@viitor.cloud>
2019-04-08 13:51:24 +0530

    Comment reformating

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -21,8 +21,6 @@ class LoginController extends Controller
     use AuthenticatesUsers;
 
     /**
-     * Where to redirect users after login.
-     *
      * @var string
      */
     protected $redirectTo = '/home';
